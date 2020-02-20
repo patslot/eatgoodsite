f6ae1532c6eac28056ae94ee5a7d4c555187c19b
patslot <patkwok@nextdigital.com.hk>
2020-02-20 18:42:07 +0800

    no message

---
+++ src/main.js
@@ -0,0 +1,60 @@
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+
+import angular from 'angular';
+
+import "popper.js";
+import "./scss/main.scss";
+
+var campaign_name = "eatgoodsite";
+
+
+
+var eatgoodsiteApp = angular.module('eatgoodsite', []);
+
+eatgoodsiteApp.controller('eatgoodsiteController', function eatgoodsiteController($scope, $timeout) {
+
+   
+
+    function gaEventcall(action, category, label, value){
+        gtag('event', action, {
+            'event_category': category,
+            'event_label': label,
+            'value': value
+        });
+    }
+    $scope.articleClick = function(a){
+        var win = window.open(a.LINK, '_blank');
+        if (win) {
+            //Browser has allowed it to be opened
+           
+            var array = a.TAG.split(",");
+            $.each(array,function(i){
+                gaEventcall('tag','article_link_tag',$.trim(array[i]),$.trim(array[i]));
+             });
+            gaEventcall('click','article_link_click',a.TITLE,a.TITLE)
+            win.focus();
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
+    }
+
+    $scope.$watch('$viewContentLoaded',function () {
+        var payload = {};
+        
+        $timeout(function () {
+           
+        }, 0);
+    });
+
+  
+});
+
+
+
+$(document).ready(function () {
+    // init();
+});
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'public/assets/'),
+        filename: 'bundle.js',
+        publicPath: './'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(s*)css$/,
+                use: extractPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(gif|jpg|png|woff|woff2|eot|ttf|svg)(\?.*)?$/,
+                loader: "url-loader?limit=100000"
+            }
+        ]
+    },
+    watch: true, 
+    plugins: [
+        extractPlugin,
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
+    ]
+};
